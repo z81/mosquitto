bb844c13f7bc456fe5d7ca89c1425b0b5fdb08cf
Alexander Iklyukov <iklyukov@ordbuy.com>
2017-01-16 19:49:08 +0300

    0.0.4

--- install.sh
+++ install.sh
@@ -9,25 +9,25 @@ apt-get -y upgrade
 apt-get install -y gcc wget mosquitto libmosquitto-dev unzip make libssl-dev libmysqlclient-dev
 
 # mosquito && mosquitto-auth-plug
-wget https://codeload.github.com/jpmens/mosquitto-auth-plug/zip/0.0.8
-unzip 0.0.8
+wget https://github.com/z81/mosquitto-auth-plug/archive/master.zip
+unzip master.zip
 wget http://mosquitto.org/files/source/mosquitto-1.4.9.tar.gz
 tar -xvzf mosquitto-1.4.9.tar.gz
 
 # install openssl
-wget http://www.openssl.org/source/openssl-1.0.1.tar.gz
-tar -xvzf openssl-1.0.1.tar.gz
-cd openssl-1.0.1
-./config --prefix=/usr/
-make
-make install
+#wget http://www.openssl.org/source/openssl-1.0.1.tar.gz
+#tar -xvzf openssl-1.0.1.tar.gz
+#cd openssl-1.0.1
+#./config --prefix=/usr/
+#make
+#make install
 
 # move config
-mv /tmp/make-auth /code/mosquitto-auth-plug-0.0.8/config.mk
+mv /tmp/make-auth /code/mosquitto-auth-plug-master/config.mk
 mv /tmp/bridge.conf /etc/mosquitto/conf.d/bridge.conf
 
 # build plugin
-cd /code/mosquitto-auth-plug-0.0.8
+cd /code/mosquitto-auth-plug-master
 make clean
 make
 
@@ -36,6 +36,6 @@ DEBIAN_FRONTEND=noninteractive && apt-get install -y mysql-server mysql-client
 service mysql start
 
 # create database
-mysql -p$MYSQL_ROOT_PASSWORD < ./mosquitto.sql
+mysql -p$MYSQL_ROOT_PASSWORD < /mosquitto.sql
 
 # create test user
